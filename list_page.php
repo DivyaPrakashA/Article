dbb640b5fce59dc345291ddd7af75769669cbb20
DivyaPrakashA <divyaprakash9964@gmail.com>
2016-06-06 04:16:05 -0700

    Delete list_page.php

--- list_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'db.php';
-include_once 'db_operations.php';
-session_start();
-$user = $_SESSION['username'];
-echo "Article By".$user;
-$con = connection();
-display_page($user,$con);
-?>
