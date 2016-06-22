0fa4a3367a758b0666054078bb386b981ff827cb
DivyaPrakashA <divyaprakash9964@gmail.com>
2016-06-22 11:25:04 -0700

    Add files via upload

--- home.php
+++ home.php
@@ -13,16 +13,28 @@ function login()
 		$pass = $_POST['password'];
 		$_SESSION['username'] = $user;
 		$con = connection();
-		$cnt = get_num_rows($user,$pass,$con);
+		$res = get_num_rows($user,$pass,$con);
+		$str = explode("||",$res);
+		$type = $str[0];
+		$cnt = $str[1];
 		/*while ($row = mysql_fetch_array($data)) {
 		print_r($row);
 		}
 		*/
+		//print_r($cnt);
+		//exit;
+		
 	}
-	if($cnt == 1)
+	if($cnt == 1 && $type == 'm')
 	{
-		echo "login successful $user<br>";
-		echo "Logging ".$_SESSION['username'];
+		//echo "login successful $user<br>";
+		//echo "Logging ".$_SESSION['username'];
+	}
+	else if($cnt == 1 && $type == 'a')
+	{
+		//echo "login successful $user<br>";
+		//echo "Logging ".$_SESSION['username'];
+		header("Location:admin.php");
 	}
 	else
 	{
@@ -101,6 +113,20 @@ function create()
 	}
 }
 
+function update()
+{
+	$con = connection();
+	if(isset($_POST['Update']))
+	{
+	$page_id = $_POST['page_id'];
+	$title = $_POST['title'];
+	$body_content = $_POST['body'];
+	$result = list_update_page($page_id,$title,$body_content,$con);
+	header("Location:list_page.php");
+	}
+	
+}
+
 function delete()
 {
 	$con = connection();
@@ -111,6 +137,29 @@ function delete()
 	$res = delete_pg($title,$name,$con);
 	}
 	header("Location:list_page.php");
+}
+
+function image_upload()
+{
+	$con = connection();
+	if(isset($_POST['upload']) && !empty($_FILES['image']['name']))
+	{
+	$image_name = $_POST['image_name'];
+    $image =  $_FILES['image']['name'];
+	$tempname = $_FILES['image']['tmp_name'];
+	$originalname = $_FILES['image']['name'];
+	$size = $_FILES['image']['size'];
+	$type = $_FILES['image']['type'];
+	if(move_uploaded_file($tempname,"images/".$originalname))
+	{
+	//echo "Image Uploaded";
+	$res = upload_image($image_name,$image,$con);
+	}
+	else
+	{
+	//echo "Image not uploaded";
+	}
+	}
 }
 
 if(isset($_POST['login']))
@@ -137,127 +186,112 @@ if(isset($_POST['delete']))
 {
 delete();
 }
-
-
+if(isset($_POST['Update']))
+{
+update();
+}
+if(isset($_POST['upload']) && !empty($_POST['image_name']))
+{
+image_upload();
+}
+if(isset($_POST['checkname']))
+{
+$con = connection();
+$username = $_POST['checkname'];
+$sql = "select user_name from users where user_name='$username'"; 
+$res = mysql_query($sql,$con);
+$cnt = mysql_num_rows($res); 
+if($cnt > 0)
+{
+echo "exist";
+exit;
+}
+else
+{
+echo "not exist";
+exit;
+}
+}
 ?>
 <html>
 <head>
-<title></title>
+<title>SignUp</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=initial-width ,  initial-scale=1.0" />
+<link href="css/bootstrap.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ek+Mukta">
-<style>
-/* CSS Document */
- 
-.clearfix:after {
-    display:block;
-    clear:both;
-}
- 
-/*----- Menu Outline -----*/
-.menu-wrap {
-    width:100%;
-    box-shadow:0px 1px 3px rgba(0,0,0,0.2);
-    background:#3e3436;
-}
- 
-.menu {
-    width:1000px;
-    margin:0px auto;
-}
- 
-.menu li {
-    margin:0px;
-    list-style:none;
-    font-family:'Ek Mukta';
-}
- 
-.menu a {
-    transition:all linear 0.15s;
-    color:#919191;
-}
- 
-.menu li:hover > a, .menu .current-item > a {
-    text-decoration:none;
-    color:#be5b70;
-}
- 
-.menu .arrow {
-    font-size:11px;
-    line-height:0%;
-}
- 
-/*----- Top Level -----*/
-.menu > ul > li {
-    float:left;
-    display:inline-block;
-    position:relative;
-    font-size:19px;
-}
- 
-.menu > ul > li > a {
-    padding:10px 40px;
-    display:inline-block;
-    text-shadow:0px 1px 0px rgba(0,0,0,0.4);
-}
- 
-.menu > ul > li:hover > a, .menu > ul > .current-item > a {
-    background:#2e2728;
-}
- 
-/*----- Bottom Level -----*/
-.menu li:hover .sub-menu {
-    z-index:1;
-    opacity:1;
-}
- 
-.sub-menu {
-    width:160%;
-    padding:5px 0px;
-    position:absolute;
-    top:100%;
-    left:0px;
-    z-index:-1;
-    opacity:0;
-    transition:opacity linear 0.15s;
-    box-shadow:0px 2px 3px rgba(0,0,0,0.2);
-    background:#2e2728;
-}
- 
-.sub-menu li {
-    display:block;
-    font-size:16px;
-}
- 
-.sub-menu li a {
-    padding:10px 30px;
-    display:block;
-}
- 
-.sub-menu li a:hover, .sub-menu .current-item a {
-    background:#3e3436;
-}
-
-
-</style>
 </head>
 <body>
-<fieldset style='width:10px'><legend> Article</legend>
-<div class="menu-wrap">
-    <nav class="menu">
-        <ul class="clearfix">
-            <li><a href="#">Home</a></li>
-            <li>
-                <a href="#">Pages <span class="arrow">&#9660;</span></a>
- 
-                <ul class="sub-menu">
-                    <li><a href="create_page.php">Create Page</a></li>
-                    <li><a href="list_page.php">List Page</a></li>
-                    <li><a href="edit_page.php">Edit Page</a></li>
-                    <li><a href="delete_page.php">Delete Page</a></li>
-                </ul>
-            </li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-    </nav>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12">
+      <?php include "menus.php"; ?>
+    </div>
+  </div>
 </div>
+<?php include "slider.php";
+?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4 col-md-4 col-lg-4"></div>
+	
+	<div class="col-sm-4 col-md-4 col-lg-4">
+      <form method="post" enctype="multipart/form-data" action="home.php">
+
+	  <div class="form-group">
+	  Article Title<input type="text" name="image_name" class="form-control">
+	  </div>
+	  <div class="form-group">
+	  Upload Article<input type="file" name="image" class="form-control">
+	  </div>
+	  <input type="submit" name="upload" value="Upload" class="btn btn-success">
+	  
+	  </form>
+	</div>
+	  <div class="col-sm-4 col-md-4 col-lg-4"></div>
+	  </div>
+	  <?php 
+	 /*
+	  $page1 = 0;
+	  
+	  
+	  $page_no = $cnt/9;
+	  $page_no = ceil($page_no);
+	  for($i=0;$i<=$page_no;$i++)
+	  {
+	  ?>
+	  <a href="home.php?page=i"><?php echo $i;
+	  }
+	  $page = $_GET['page'];
+	  if($page == 0 || $page == 1)
+	  { $page1 = 0; }
+	  else { $page1 = ($page*9)-9; }*/
+	  
+	  $con = connection();
+	  $img = display_image($con);
+	  $str1 = explode('||',$img);
+	  $res = $str1[0];
+	  $cnt = $str1[1];
+	  $img_src = "http://localhost/Article2/images/";
+	  ?>
+	  <div class="row">
+	  <?php 
+	  while($row = mysql_fetch_array($res))
+	  {?>
+	  <div class="col-sm-3 col-md-3 col-lg-3 image">
+	  <img src="<?php echo $img_src.$row['image'] ?>" width ="100%" height="25%" class="img-rounded">
+	  </div>
+      
+	 <?php }
+	  ?>
+	  </div>
+  </div>
+<?php include "footer.php";
+?>
 </body>
 </html>
